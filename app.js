63ebe78c596e0ee3e32e7b6fda34691c72ac928e
Muhammad Sami <134046729+samihere14@users.noreply.github.com>
2024-03-04 17:05:13 +0500

    Array Basics

---
+++ app.js
@@ -0,0 +1,61 @@
+// Array
+// Chapter 14-16
+//1. Declare an empty array using JS literal notation to store student names in future.
+
+let studentNames = [];
+
+//2. Declare an empty array using JS object notation to store student names in future.
+
+const studentData = {
+    studentNames: []
+};
+
+// 3. Declare and initialize a strings array.
+
+const fruits = ["Apple", "Banana", "Pinapple", "Grape"];{
+    console.log(fruits)
+}
+
+// 4. Declare and initialize a numbers array.
+
+const number = ["1", "2", "3", "4"];{
+    console.log(number)
+}
+
+// 1. Declare an empty array.
+
+const empArray = [];
+
+//2. Code an array with 1 string element
+
+const myArray = ["Hello, World!"];
+console.log(myArray[0]);
+
+// 3. var alphabet = [&quot;h&quot;,&quot;i&quot;,&quot;j&quot;,&quot;k&quot;]. Now print the letter “j” in alert using array index
+
+var alphabet = ["h", "i", "j", "k"];
+var letterJ = alphabet[2];
+
+alert(letterJ);
+
+// 4. var alphabet=[&quot;h&quot;,&quot;i&quot;,&quot;j&quot;,&quot;k&quot;, “l”,”m”, “n”, “o”]. Find the total length of array.
+
+var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"];
+var arrayLength = alphabet.length;
+
+console.log(arrayLength);
+
+// 5. Declare an array with one element and Add a second element with index in array. Create an alert whose message is the new element.
+
+var array = ["First Element"]; 
+
+myArray[1] = "Second Element"; 
+
+var newElement = myArray[1]; 
+
+alert(newElement);
+
+//1. Code an array with 1 string element.
+
+const myArray = ["Hello, World!"];
+console.log(myArray[0]);
